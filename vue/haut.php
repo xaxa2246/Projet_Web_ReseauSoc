3b0ead38fea09807c633ff7bfa327d4da9b1ecb7
billybob1991 <xavier.leclercq@outlook.be>
2017-02-17 19:04:46 +0100

    1ere version
    Version de l'exercice 3 téléchargée

---
+++ vue/haut.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Montitre</title>
+	<link rel="stylesheet" href="../vue/css/page.css">
+
+</head>
+<body>
+	<header id="header" class="header">
+		<H1>
+			Collège Technique "Aumôniers du Travail" <br>
+			Enseignement de Promotion Sociale<br>
+		</H1>
+		<?php if(isset($_SESSION['NOM'])){ echo 'Bonjour Mr. '.$_SESSION['NOM'];} ?>
+		<ul>
+			<li><a href="../control/page1.php" >Acceuil</a></li>
+			<li><a href="../control/page2.php" >Contact</a></li>
+			<li><a href="../control/destroy.php" >Logout</a></li>
+			<li><a href="../control/employees_tab.php" >Employés</a></li>
+			<li><a href="../control/clients_tab.php" >Clients</a></li>
+			<li><a href="../control/suppliers_tab.php" >Fourniseurs</a></li>
+		</ul>
+		
+	</header>
