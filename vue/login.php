5fa8409152288d96da20cf19b88f6a6d99394fd8
billybob1991 <xavier.leclercq@outlook.be>
2017-02-24 19:58:43 +0100

    3 eme version
    modif login.php

--- vue/login.php
+++ vue/login.php
@@ -1,9 +1,13 @@
 <section>
 	<article>
 		<form action="../control/login_control.php" method="post" accept-charset="utf-8">
-			<input type="text" name="UTILISATEUR" value="" placeholder="nom.prénom@fournisseur.com" required>
+		<p> Utilisateur  
+			<input type="text" name="UTILISATEUR" value="" placeholder="Utilisateur" required>
+			</p>
+			<p> Mot de passe
 			<input type="password" name="CODE" value="" placeholder="Mot de passe" required>
-			<input type="submit" name="" value="Go">
+			</p>
+			<input type="submit" name="" value="Valider">
 		</form>
 		<?php if (isset($_SESSION['ERROR_LOGIN'])){
 			echo "<p>".$_SESSION['ERROR_LOGIN']."</p>";
